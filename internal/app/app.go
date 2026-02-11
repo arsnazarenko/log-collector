31042f9db684414b7faa816a6f3aac7be9aea308
arsnazarenko <arsnazarenko@gmail.com>
2026-02-11 18:40:15 +0300

    fix: nil deref in usecase

--- internal/app/app.go
+++ internal/app/app.go
@@ -16,16 +16,14 @@ import (
 	"github.com/arsnazarenko/log-collector/internal/config"
 	v1 "github.com/arsnazarenko/log-collector/internal/controller/http/v1"
 	"github.com/arsnazarenko/log-collector/internal/repo/persistent"
+	log_usecase "github.com/arsnazarenko/log-collector/internal/usecase/log"
+	"github.com/arsnazarenko/log-collector/pkg/clickhouse"
+	rmq_consumer "github.com/arsnazarenko/log-collector/pkg/rabbitmq"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	oapi_middleware "github.com/oapi-codegen/nethttp-middleware"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/arsnazarenko/log-collector/internal/usecase"
-	log_usecase "github.com/arsnazarenko/log-collector/internal/usecase/log"
-	"github.com/arsnazarenko/log-collector/pkg/clickhouse"
-	rmq_consumer "github.com/arsnazarenko/log-collector/pkg/rabbitmq"
 	"github.com/wagslane/go-rabbitmq"
 )
 
@@ -55,39 +53,21 @@ func Run() {
 	}
 	swagger.Servers = nil
 
-	rmq, err := rmq_consumer.New(cfg.RabbitMQ)
+	ctx := context.Background()
+	ch, err := clickhouse.New(cfg.Clickhouse)
 	if err != nil {
-		log.Fatalf("Error connecting to rabbitmq: %s", err)
+		log.Fatalf("Error connecting to clickhouse: %s", err)
 	}
-	defer rmq.Close()
+	defer ch.Close()
 
-	err = rmq.DeclareExchange()
-	if err != nil {
-		log.Fatalf("Error declaring exchange: %s", err)
-	}
-
-	err = rmq.DeclareQueue()
-	if err != nil {
-		log.Fatalf("Error declaring queue: %s", err)
+	logRepo := persistent.NewLogClickhouseRepo(ch)
+	if err = logRepo.CreateTable(ctx); err != nil {
+		log.Fatalf("Clickhouse error: %s", err)
 	}
 
-	var logUsecase usecase.LogUsecase
-
-	rmq.Consume(func(d rabbitmq.Delivery) rabbitmq.Action {
-		var logInput gen.LogEntryInput
-		if err = json.Unmarshal(d.Body, &logInput); err != nil {
-			log.Printf("Failed to parse log entry from RabbitMQ: %v", err)
-			return rabbitmq.NackDiscard
-		}
-
-		if _, err = logUsecase.AddLogs(context.Background(), []gen.LogEntryInput{logInput}); err != nil {
-			log.Printf("Failed to save log from RabbitMQ: %v", err)
-			return rabbitmq.NackDiscard
-		}
-
-		log.Printf("Saved log from RabbitMQ: %s", logInput.Message)
-		return rabbitmq.Ack
-	})
+	logUsecase := log_usecase.NewLogUsecase(logRepo)
+	serverImpl := v1.NewLogServer(logUsecase)
+	server := gen.NewStrictHandler(serverImpl, []gen.StrictMiddlewareFunc{})
 
 	r := chi.NewRouter()
 
@@ -105,21 +85,6 @@ func Run() {
 	r.Handle("/swagger/*", http.StripPrefix("/swagger/", http.FileServer(http.FS(gen.SwaggerUI))))
 	r.Handle("/metrics", promhttp.Handler())
 
-	ctx := context.Background()
-	ch, err := clickhouse.New(cfg.Clickhouse)
-	if err != nil {
-		log.Fatalf("Error connecting to clickhouse: %s", err)
-	}
-	defer ch.Close()
-	logRepo := persistent.NewLogClickhouseRepo(ch)
-	if err := logRepo.CreateTable(ctx); err != nil {
-		log.Fatalf("Clickhouse error: %s", err)
-	}
-
-	logUsecase = log_usecase.NewLogUsecase(logRepo)
-	serverImpl := v1.NewLogServer(logUsecase)
-	server := gen.NewStrictHandler(serverImpl, []gen.StrictMiddlewareFunc{})
-
 	r.Route("/", func(r chi.Router) {
 		r.Use(oapi_middleware.OapiRequestValidator(swagger))
 		r.Use(middleware.Logger)
@@ -131,6 +96,37 @@ func Run() {
 		Handler: r,
 		Addr:    net.JoinHostPort(cfg.Host, cfg.Port),
 	}
+
+	rmq, err := rmq_consumer.New(cfg.RabbitMQ)
+	if err != nil {
+		log.Fatalf("Error connecting to rabbitmq: %s", err)
+	}
+	defer rmq.Close()
+
+	err = rmq.DeclareExchange()
+	if err != nil {
+		log.Fatalf("Error declaring exchange: %s", err)
+	}
+
+	err = rmq.DeclareQueue()
+	if err != nil {
+		log.Fatalf("Error declaring queue: %s", err)
+	}
+	rmq.Consume(func(d rabbitmq.Delivery) rabbitmq.Action {
+		var logInput gen.LogEntryInput
+		if err = json.Unmarshal(d.Body, &logInput); err != nil {
+			log.Printf("Failed to parse log entry from RabbitMQ: %v", err)
+			return rabbitmq.NackDiscard
+		}
+
+		if _, err = logUsecase.AddLogs(context.Background(), []gen.LogEntryInput{logInput}); err != nil {
+			log.Printf("Failed to save log from RabbitMQ: %v", err)
+			return rabbitmq.NackDiscard
+		}
+
+		log.Printf("Saved log from RabbitMQ: %s", logInput.Message)
+		return rabbitmq.Ack
+	})
 
 	go func() {
 		sigs := make(chan os.Signal, 1)
